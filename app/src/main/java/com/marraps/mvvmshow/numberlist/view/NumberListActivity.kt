e1b9f63495ba4027ed5eeb3d5a32e6b99adb6bbb
mnetoae <rafaelmarraneto@correio.itaucom.br>
2020-03-11 11:13:01 -0300

    faz as chamadas de api de uma maneira questionavel

--- app/src/main/java/com/marraps/mvvmshow/numberlist/view/NumberListActivity.kt
+++ app/src/main/java/com/marraps/mvvmshow/numberlist/view/NumberListActivity.kt
@@ -5,15 +5,17 @@ import android.view.View
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.marraps.mvvmshow.R
-import com.marraps.mvvmshow.ViewModelFactory
-import com.marraps.mvvmshow.numberlist.viewmodel.NumberListViewModel
+import com.marraps.mvvmshow.numberlist.model.NumbersResponse
+import com.marraps.mvvmshow.numberlist.model.NumbersServiceListener
+import com.marraps.mvvmshow.retrofit.RetrofitService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class NumberListActivity : AppCompatActivity() {
+class NumberListActivity : AppCompatActivity(), NumbersServiceListener {
 
     private val numberList: RecyclerView by lazy {
         findViewById<RecyclerView>(R.id.rv_numbers_list)
@@ -23,20 +25,14 @@ class NumberListActivity : AppCompatActivity() {
         findViewById<ProgressBar>(R.id.pb_list_loading)
     }
 
-    private val viewModel by lazy {
-        ViewModelProviders
-                .of(this, ViewModelFactory())
-                .get(NumberListViewModel::class.java)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_number_list)
 
         setList()
-        setObservers()
 
-        viewModel.getNumbers()
+        showLoading()
+        getNumbers(this)
     }
 
     private fun setList() {
@@ -47,30 +43,44 @@ class NumberListActivity : AppCompatActivity() {
         }
     }
 
-    private fun setObservers() {
-        viewModel.command.observe(this, Observer {
-            when (it) {
-                is NumberListViewModel.Command.ShowLoading -> showLoading()
-                is NumberListViewModel.Command.HideLoading -> hideLoading()
-            }
-        })
-
-        viewModel.numberList.observe(this, Observer {
-            numberList.adapter = NumbersListAdapter(it)
-        })
-
-        viewModel.numberError.observe(this, Observer {
-            Toast.makeText(this, R.string.error_message, Toast.LENGTH_LONG).show()
-        })
-    }
-
-    private fun showLoading(){
+    private fun showLoading() {
         numberList.visibility = View.GONE
         progressBar.visibility = View.VISIBLE
     }
 
-    private fun hideLoading(){
+    private fun hideLoading() {
         numberList.visibility = View.VISIBLE
         progressBar.visibility = View.GONE
+    }
+
+    private fun getNumbers(listener: NumbersServiceListener) {
+
+        val call = RetrofitService.getService().getNumbers()
+        call.enqueue(object : Callback<NumbersResponse> {
+
+            override fun onResponse(
+                call: Call<NumbersResponse>,
+                response: Response<NumbersResponse>
+            ) {
+                response.body()?.let {
+                    listener.onSuccess(it)
+
+                } ?: listener.onError(Exception())
+            }
+
+            override fun onFailure(call: Call<NumbersResponse>, t: Throwable) {
+                listener.onError(t)
+            }
+        })
+    }
+
+    override fun onSuccess(response: NumbersResponse) {
+        numberList.adapter = NumbersListAdapter(response.numbers)
+        hideLoading()
+    }
+
+    override fun onError(error: Throwable) {
+        Toast.makeText(this, R.string.error_message, Toast.LENGTH_LONG).show()
+        hideLoading()
     }
 }
